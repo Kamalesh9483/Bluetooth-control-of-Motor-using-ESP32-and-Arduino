5d6f00bcc204e1a895eff3328aa17c85e1b127c0
Kamalesh <kamalesh9483@gmail.com>
2021-05-12 19:50:41 +0530

    Added 3 different speed controls and 1 OFF control for motor using bluetooth module of ESP32

--- Bluetooth_motor_control.ino
+++ Bluetooth_motor_control.ino
@@ -11,26 +11,44 @@ BluetoothSerial SerialBT;
 int received;
 char receivedChar;
 
-const char turnON = 'a';
-const char turnOFF = 'b';
+const char speed1 = '1';
+const char speed2 = '2';
+const char speed3 = '3';
+const char turnOFF = 'F';
+
 
 // Motor A pin setup
 const int IN_1 = 27;
 const int IN_2 = 26;
-// const int EN_A = 14;
+const int EN_A = 14;
+
+// PWM setup
+int freq = 115200;
+int pwmChannel = 0;
+int resolution = 8;
+int dutyCycle = 120;
+
 
 void setup() {
  
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  SerialBT.begin("Bluetooth_Motor_Control"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
-  Serial.println("To turn on motor type: a "); // print on serial monitor
-  Serial.println("To turn off motor type: b "); // print on serial monitor
+  Serial.println("To run motor at speed1 type: 1 "); // print on serial monitor
+  Serial.println("To run motor at speed2 type: 2  "); // print on serial monitor
+  Serial.println("To run motor at speed3 type: 3 "); // print on serial monitor
+  Serial.println("To off motor type: F "); // print on serial monitor
 
    // Motor pins
   pinMode(IN_1, OUTPUT);
   pinMode(IN_2, OUTPUT);
-  // pinMode(EN_A, OUTPUT);
+  pinMode(EN_A, OUTPUT);
+
+  // PWM setup
+  ledcSetup(pwmChannel, freq, resolution);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(EN_A, pwmChannel);
 }
 
 void loop() {
@@ -47,27 +65,58 @@ void loop() {
     Serial.print("Received"); // write the text in Serial monitor
     Serial.println(receivedChar); // write the text in serial monitor
 
-    if(receivedChar == turnON)
+  // speed1 conditional statement
+    if(receivedChar == speed1)
       {
-        SerialBT.println("Motor ON");
-        Serial.println("Motor ON");
+        SerialBT.println("Motor ON with dutyCycle:");
+        SerialBT.println(dutyCycle);
+        Serial.println("Motor ON with dutyCycle:");
+        Serial.println(dutyCycle);
     
-         // Move DC motor 
-          digitalWrite(IN_1, HIGH);
-          digitalWrite(IN_2, LOW);
+       // DC motor with speed1
+        digitalWrite(IN_1, HIGH);
+        digitalWrite(IN_2, LOW);          
+        ledcWrite(pwmChannel, dutyCycle);      
       }
 
-     else if(receivedChar == turnOFF)
+ // speed2 conditional statement
+     else if(receivedChar == speed2)
+      {
+        SerialBT.println("Motor ON with dutyCycle:");
+        SerialBT.println(dutyCycle+55);
+        Serial.println("Motor ON with dutyCycle:");
+        Serial.println(dutyCycle+55);
+    
+       // DC motor with speed1
+        digitalWrite(IN_1, HIGH);
+        digitalWrite(IN_2, LOW);          
+        ledcWrite(pwmChannel, (dutyCycle+55)); 
+      }
+
+ // speed3 conditional statement
+      else if(receivedChar == speed3)
+      {
+        SerialBT.println("Motor ON with dutyCycle:");
+        SerialBT.println(dutyCycle+120);
+        Serial.println("Motor ON with dutyCycle:");
+        Serial.println(dutyCycle+120);
+    
+       // DC motor with speed1
+        digitalWrite(IN_1, HIGH);
+        digitalWrite(IN_2, LOW);          
+        ledcWrite(pwmChannel, (dutyCycle+120)); 
+      }
+
+ // Motor OFF conditional statement
+      else if(receivedChar == turnOFF)
       {
         SerialBT.println("Motor OFF");
         Serial.println("Motor OFF");
     
-         // OFF the DC Motor
-
+       // DC motor with speed1
         digitalWrite(IN_1, LOW);
-        digitalWrite(IN_2, LOW);   
-      }
-    
+        digitalWrite(IN_2, LOW);          
+      } 
   }
   delay(20);
 }
